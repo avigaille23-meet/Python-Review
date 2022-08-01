e7a28f5feb7912e4435a050fc69c4c1bbc4de4eb
avigaille23-meet <avigaillevite@gmail.com>
2022-08-01 23:31:32 +0300

    aa

---
+++ python.py
@@ -0,0 +1,22 @@
+def create_youtube_video(name, description):
+	video={"title":title, "description":description, "like":0, "dislike":0, "comment":{"username": ""}}
+	return video
+
+def like (video):
+	if like in video:
+		video["like"]= video["like"]+1
+	return like
+
+def dislike (video):
+	if dislike in video:
+		video["dislike"]= video["dislike"]+1
+	return dislike
+
+
+
+
+
+
+
+
+
